6df9f4764515ca6e8289f312da666fc4c40cc9a3
Binyamin Aron Green <Binyaminagreen@gmail.com>
2020-05-21 12:55:53 -0400

    handle errors

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ PyConvert
 """
 
 import gi
+
 gi.require_version("Gtk", "3.0")
 
 from gi.repository import Gtk, GdkPixbuf, GLib
@@ -12,9 +13,14 @@ class Gui(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self)
         GLib.set_application_name('PyConvert')
+
+        icon_size = Gtk.IconSize.lookup(Gtk.IconSize.SMALL_TOOLBAR)
+        self.icon = GdkPixbuf.Pixbuf.new_from_file_at_size("logo.svg", icon_size[1], icon_size[2])
+
+        self.set_default_icon(self.icon)
         self.set_title("PyConvert")
-        self.set_default_icon_from_file("logo.svg")
-        self.set_border_width(8)
+        self.set_border_width(10)
+
         self.convert_usd = {
             "CAD": {"name": "Canadian Dollar", "rate": 1.33, "prefix": "$"},
             "EUR": {"name": "Euro", "rate": 0.92, "prefix": "€"},
@@ -24,8 +30,12 @@ class Gui(Gtk.Window):
         }
 
         self.header_bar()
+        # self.set_interactive_debugging(True)
 
         self.grid = Gtk.Grid()
+        # print(dir(self.grid))
+        self.grid.set_row_spacing(6)
+        self.grid.set_column_spacing(6)
         
         self.entry = Gtk.SpinButton.new_with_range(0, 9999, 1.00)
         self.entry.set_digits(2)
@@ -53,16 +63,34 @@ class Gui(Gtk.Window):
         self.add(self.grid)
 
     def convert(self, *args):
-        current_dict = self.convert_usd[self.dropdown.props.active_id]
         current_input = float(self.entry.get_value())
-        
-        self.output.set_text(current_dict.get("prefix") + ' ' + str(current_input * current_dict.get("rate")))
+        current_dict = self.convert_usd[self.dropdown.props.active_id]  if self.dropdown.props.active_id else None
 
+        if not current_input:
+            # Show Error
+            popover = Gtk.Popover.new(relative_to=self.entry)
+            popover.set_border_width(4)
+            popover.add(Gtk.Label(label="Input should be greater than zero", visible=True))
+            popover.popup()
+            popover.connect("closed", lambda widget: widget.destroy())
+        elif not current_dict:
+            # Show Error
+            popover = Gtk.Popover.new(relative_to=self.dropdown)
+            popover.set_border_width(4)
+            popover.add(Gtk.Label(label="Choose a Currency", visible=True))
+            popover.popup()
+            popover.connect("closed", lambda widget: widget.destroy())
+        else:
+            # Convert
+            self.output.set_text(current_dict.get("prefix") + ' ' + str(current_input * current_dict.get("rate")))
     def header_bar(self):
         widget = Gtk.HeaderBar()
         widget.set_show_close_button(True)
-        widget.props.title = self.props.title
-
+        widget.set_title(self.props.title);
+        
+        # Theme should govern whether icon shows
+        # widget.pack_start(Gtk.Image.new_from_pixbuf(self.icon))
+        
         about_icon = Gtk.Button.new_from_icon_name("help-about", 2)
         about_icon.connect("clicked", self.about_dialog)
         about_icon.set_tooltip_text("About this Program")
