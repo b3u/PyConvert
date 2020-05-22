5e20322d15110deedb9adabdd8c54c8691bd66fe
Binyamin Aron Green <Binyaminagreen@gmail.com>
2020-05-22 14:42:34 -0400

    format number as money

--- Main.py
+++ Main.py
@@ -8,7 +8,6 @@ import gi
 gi.require_version("Gtk", "3.0")
 
 from gi.repository import Gtk, GdkPixbuf, GLib
-
 class Gui(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self)
@@ -33,11 +32,10 @@ class Gui(Gtk.Window):
         # self.set_interactive_debugging(True)
 
         self.grid = Gtk.Grid()
-        # print(dir(self.grid))
         self.grid.set_row_spacing(6)
         self.grid.set_column_spacing(6)
         
-        self.entry = Gtk.SpinButton.new_with_range(0, 9999, 1.00)
+        self.entry = Gtk.SpinButton.new_with_range(0, 9999, 0.10)
         self.entry.set_digits(2)
 
         self.grid.attach(self.entry, 0, 0, 1, 1)
@@ -64,31 +62,24 @@ class Gui(Gtk.Window):
 
     def convert(self, *args):
         current_input = float(self.entry.get_value())
-        current_dict = self.convert_usd[self.dropdown.props.active_id]  if self.dropdown.props.active_id else None
-
+        
+        # Handle Errors
         if not current_input:
-            # Show Error
-            popover = Gtk.Popover.new(relative_to=self.entry)
-            popover.set_border_width(4)
-            popover.add(Gtk.Label(label="Input should be greater than zero", visible=True))
-            popover.popup()
-            popover.connect("closed", lambda widget: widget.destroy())
-        elif not current_dict:
-            # Show Error
-            popover = Gtk.Popover.new(relative_to=self.dropdown)
-            popover.set_border_width(4)
-            popover.add(Gtk.Label(label="Choose a Currency", visible=True))
-            popover.popup()
-            popover.connect("closed", lambda widget: widget.destroy())
-        else:
-            # Convert
-            self.output.set_text(current_dict.get("prefix") + ' ' + str(current_input * current_dict.get("rate")))
+            return self.show_popover(self.entry, "Input should be greater than zero")
+        elif not self.dropdown.props.active_id:
+            return self.show_popover(self.dropdown, "Choose a currency")
+
+        # Calculate
+        current_dict = self.convert_usd[self.dropdown.props.active_id] 
+        amount = round(current_input * current_dict.get("rate"), 2)
+        self.output.set_text(current_dict.get("prefix") + ' ' + format(amount, '.2f'))
+
     def header_bar(self):
         widget = Gtk.HeaderBar()
         widget.set_show_close_button(True)
         widget.set_title(self.props.title);
         
-        # Theme should govern whether icon shows
+        # Theme should govern whether app icon shows
         # widget.pack_start(Gtk.Image.new_from_pixbuf(self.icon))
         
         about_icon = Gtk.Button.new_from_icon_name("help-about", 2)
@@ -97,14 +88,23 @@ class Gui(Gtk.Window):
         widget.pack_end(about_icon)
 
         self.set_titlebar(widget)
+    def show_popover(self, parent, text):
+        popover = Gtk.Popover.new(relative_to=parent)
+        popover.set_border_width(4)
+        popover.add(Gtk.Label(label=text, visible=True))
+        popover.popup()
+        popover.connect("closed", lambda widget: widget.destroy())
 
     def about_dialog(self, *args):
         widget = Gtk.AboutDialog()
-        widget.set_license_type(7)
+        widget.set_license_type(Gtk.License.MIT_X11)
+        widget.set_authors(["Binyamin Green https://binyam.in"])
         widget.set_version("1.0.0")
+        widget.set_website("https://gitlab.com/b3u/pyconvert/")
+        widget.set_website_label("Source Code")
         widget.set_logo(GdkPixbuf.Pixbuf.new_from_file_at_size('logo.svg', 48, 48))
         widget.connect("response", lambda w, b: w.close())
-        widget.show()
+        widget.run()
 
 if __name__ == "__main__":
     app = Gui()
--- readme.md
+++ readme.md
@@ -1,6 +1,5 @@
 # PyConvert
 **Needs help with...**
   - Design ideas
-  - How to format result as money
 
 ![Example Image](./demo.png)
